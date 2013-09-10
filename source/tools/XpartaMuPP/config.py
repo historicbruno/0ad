ea601bd132bef0c7b6de5e52f82635b5a4f67e88
Peter Cannici <turkchess123@gmail.com>
2013-09-09 21:19:25 -0600

    Use scythetwirler's Elo implementation.

--- source/tools/XpartaMuPP/config.py
+++ source/tools/XpartaMuPP/config.py
@@ -10,3 +10,13 @@ leaderboard_minimum_games = 10
 # qualify as an active player.
 leaderboard_active_games = 5
 
+# Difference between two ratings such that it is
+# regarded as a "sure win" for the higher player.
+# No points are gained or lost for such a game.
+elo_sure_win_difference = 600
+
+# Lower ratings "move faster" and change more
+# dramatically than higher ones. Anything rating above
+# this value moves at the same rate as this value.
+elo_k_factor_constant_rating = 2200
+
